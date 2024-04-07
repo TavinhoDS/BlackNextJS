6c325e7dfd58a7bafc9083d5f3cb9071a82c2a0d
Otávio Duarte Sá <otavioduartesaa@gmail.com>
2024-04-06 22:13:09 -0300

    Projeto Nextjs

---
+++ src/pages/products.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+import Head from "next/head"
+import '../styles/Home.module.css'
+
+
+const Products: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Nossos Produtos</title>
+                <meta name="description" content="Conheça todos os nossos produtos" />
+                <link rel="icon" href="/favicon.ico" />
+
+            </Head>
+
+            <h1>
+                Nossos Produtos
+            </h1>
+
+        </>
+    )
+}
+
+export default Products
